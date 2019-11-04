b7343c3d60ee9fcee27c54fba4801a57c29ad5ff
Manpreet <kaurmanpreet5952@gmail.com>
2019-11-04 22:12:31 +1300

    Add project files.

---
+++ RaceDay/Balbir.cs
@@ -0,0 +1,12 @@
+﻿namespace RaceDay
+{
+    class Balbir : Punter
+    {
+        public Balbir() // Balbir class inherting the feature of Punter class
+        {
+            PunterName = "Balbir"; // Balbir Constructor
+            CashOnHand = 50; // Punter name
+            Alias = "Balbir"; // Punter Nick Name
+        }
+    }
+}
---
+++ RaceDay/Sameer.cs
@@ -0,0 +1,12 @@
+﻿namespace RaceDay
+{
+    class Sameer : Punter // Sameer class inherting the feature of Punter class
+    {
+        public Sameer() // Sameer Constructor
+        {
+            PunterName = "Sameer"; // Punter name
+            CashOnHand = 50; // Initial amount for punter
+            Alias = "Sameer"; // Punter Nick Name
+        }
+    }
+}
